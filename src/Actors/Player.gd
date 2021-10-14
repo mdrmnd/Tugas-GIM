3e2929bef4ea27532bf6409e79353728f4158d10
obediqbal <obed.ubaidillah@gmail.com>
2021-10-14 08:45:15 +0700

    added friction to player

--- src/Actors/Player.gd
+++ src/Actors/Player.gd
@@ -26,12 +26,14 @@ func _process(delta: float) -> void:
 		_animated_sprite.play("idle")
 """
 func _physics_process(delta: float) -> void:
-	move_and_slide(velocity, Vector2.UP)
 	var direction := get_direction()
 	# Cek apakah player melepas tombol jump dan player sedang di udara
 	var is_jump_interrupted := Input.is_action_just_released("Jump") and velocity.y<0
 	
-	velocity.x = speed.x * direction.x
+	if direction.x==0:
+		velocity.x = lerp(velocity.x, 0, 0.1)
+	else:
+		velocity.x = lerp(velocity.x, speed.x * direction.x, 1.0)
 	velocity.y += gravity * delta
 	
 	if direction.y == -1.0:
@@ -42,8 +44,9 @@ func _physics_process(delta: float) -> void:
 		velocity.y = 0.0
 	if Input.is_action_pressed("Plunge"):
 		velocity.y = 750
-
+		
 	play_animation(velocity)
+	move_and_slide(velocity, Vector2.UP)
 
 
 # Ambil arah dari vektor berdasarkan input
@@ -61,10 +64,11 @@ func play_animation(linear_velocity: Vector2) -> void:
 	elif linear_velocity.x<0:
 		_animated_sprite.flip_h = true
 	if is_on_floor():
-		if linear_velocity.x!=0:
-			_animated_sprite.play("run")
-		elif Input.is_action_just_released("Right") or Input.is_action_just_released("Left"):
-			_animated_sprite.play("idle_transition")
+		if abs(linear_velocity.x)>50:
+			if Input.get_action_strength("Right")==0 and Input.get_action_strength("Left")==0:
+				_animated_sprite.play("idle_transition")
+			else:
+				_animated_sprite.play("run")
 		else:
 			_animated_sprite.play("idle")
 	else:
